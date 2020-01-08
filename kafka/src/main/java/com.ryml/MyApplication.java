23830748f3915442c1433b5597d4fdf2d5397ef6
yibo.liu <1501986252@qq.com>
2020-01-08 10:31:38 +0800

    kafka

---
+++ kafka/src/main/java/com.ryml/MyApplication.java
@@ -0,0 +1,20 @@
+package com.ryml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description:
+ *
+ * @author 刘一博
+ * @version V1.0
+ * @date 2020/1/8 10:25
+ */
+@SpringBootApplication
+public class MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+}
